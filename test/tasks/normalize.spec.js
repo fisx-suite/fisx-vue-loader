c87ce2b1d3cf5a65ef361167edb9d94524aa1a2d
wuhy <sparklewhy@gmail.com>
2016-12-02 20:31:27 +0800

    update normalize

--- test/tasks/normalize.spec.js
+++ test/tasks/normalize.spec.js
@@ -4,20 +4,20 @@ var normalize = require('../../lib/normalize');
 describe('Normalize Utilities', function () {
     it('should normalize lib path', function () {
         var result = normalize.lib('a');
-        expect(result).to.eql('lib/a');
+        expect(result).to.eql('fisx-vue-loader/lib/a');
 
         result = normalize.lib('a\\b');
-        expect(result).to.eql('lib/a/b');
+        expect(result).to.eql('fisx-vue-loader/lib/a/b');
     });
 
     it('should normalize dep path', function () {
         var result = normalize.dep('a/c');
-        expect(result).to.eql('../a/c');
+        expect(result).to.eql('a/c');
 
         result = normalize.dep('a\\c');
-        expect(result).to.eql('../a/c');
+        expect(result).to.eql('a/c');
 
         result = normalize.dep('vue-hot-reload-api');
-        expect(result).to.eql('node_modules/vue-hot-reload-api');
+        expect(result).to.eql('fisx-vue-loader/node_modules/vue-hot-reload-api');
     });
 });
