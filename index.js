8684dda77e46bc0f0a4b85c121cb00e717a10a18
wuhy <sparklewhy@gmail.com>
2016-12-06 11:25:37 +0800

    extract vue component compile core

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 /**
  * @file 入口模块
- *       基于 https://github.com/vuejs/vueify 修改
  * @author sparklewhy@gmail.com
  */
 
-module.exports = exports = require('./lib/loader');
+var vueCompilerCore = require('vue-compiler-core');
+
+module.exports = exports = {
+    compiler: vueCompilerCore.compiler,
+    compile: function (filePath, content, options) {
+        var assign = require('object-assign');
+        var opts = assign({}, options, {
+            hostPkgName: require('./package.json').name
+        });
+
+        if (!opts.template) {
+            opts.template = {};
+        }
+        if (opts.template.compileToRender !== false) {
+            opts.template.compileToRender = require('./lib/template-compiler');
+        }
+
+        var parser = require('./lib/parser');
+        var parts = vueCompilerCore.parse(filePath, content, parser);
+        return vueCompilerCore.compile(parts, opts);
+    }
+};
