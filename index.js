a9ec685c17729f0793bd44d1a8ba056e9216abfc
wuhy <sparklewhy@gmail.com>
2016-12-19 19:39:21 +0800

    add insert css lib and hot reload api dir init

--- index.js
+++ index.js
@@ -5,12 +5,60 @@
 
 var vueCompilerCore = require('vue-compiler-core');
 
+var insertCssLibDir = null;
+function getInsertCssLibDir() {
+    if (insertCssLibDir != null) {
+        return insertCssLibDir;
+    }
+
+    var pkgName = require('./package.json').name;
+    var path = require('path');
+    var corePath = path.join(__dirname, 'node_modules', 'vue-compiler-core');
+    var result;
+    try {
+        require.resolve(corePath);
+        result = path.join(pkgName, 'node_modules');
+    }
+    catch (ex) {
+        result = '';
+    }
+
+    insertCssLibDir = result;
+
+    return result;
+}
+
+var hotReloadApiDir = null;
+function getHotReloadApiDir() {
+    if (hotReloadApiDir != null) {
+        return insertCssLibDir;
+    }
+
+    var pkgName = require('./package.json').name;
+    var path = require('path');
+    var hotReloadApiPkg = 'vue-hot-reload-api';
+    var corePath = path.join(__dirname, 'node_modules', hotReloadApiPkg);
+    var result;
+    try {
+        require.resolve(corePath);
+        result = path.join(pkgName, 'node_modules', hotReloadApiPkg);
+    }
+    catch (ex) {
+        result = hotReloadApiPkg;
+    }
+
+    hotReloadApiDir = result;
+
+    return result;
+}
+
 module.exports = exports = {
     compiler: vueCompilerCore.compiler,
     compile: function (filePath, content, options) {
         var assign = require('object-assign');
         var opts = assign({}, options, {
-            hostPkgName: require('./package.json').name
+            insertCssLibDir: getInsertCssLibDir(),
+            hotReloadApiDir: getHotReloadApiDir()
         });
 
         if (!opts.template) {
